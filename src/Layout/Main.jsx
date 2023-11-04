63fd84d49873a2827690bfe12ea05b101654c06a
Md. Jakir Hasan Rony <jakir15-14584@diu.edu.bd>
2023-11-04 23:43:53 +0600

    footer section added with  releavent information

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../SharedComponents/Footer/Footer";
 
 const Main = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
