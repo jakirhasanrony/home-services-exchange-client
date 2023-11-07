a52d53993d712fc61e92a3a394089c532d4986e4
Md. Jakir Hasan Rony <jakir15-14584@diu.edu.bd>
2023-11-07 21:12:33 +0600

    services page added and more services button added

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from "react";
+import ServiceCard from "../Home/ServiceCard/ServiceCard";
+
 
 const Services = () => {
+
+    const [services, setServices] = useState([]);
+    const [displayedServices, setDisplayedServices] = useState(4); 
+    const [showAllServices, setShowAllServices] = useState(false);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, []);
+    const handleShowAllServices = () => {
+        setDisplayedServices(services.length);
+        setShowAllServices(true);
+    };
+
     return (
-        <div>
-           <h2>services</h2> 
+        <div className="my-10">
+           
+            <div className="m-10 lg:px-28 grid grid-cols-1 md:grid-cols-2 gap-6 justify-center items-center">
+                {
+                    services.slice(0, displayedServices).map(service => <ServiceCard
+                        key={service.service_id}
+                        service={service}
+                    > </ServiceCard>)
+                }
+            </div>
+            {!showAllServices && services.length > displayedServices && (
+                <div className="text-center">
+                    <button
+                        onClick={handleShowAllServices}
+                        className="btn btn-outline bg-black text-white w-1/2"
+                    >
+                        More Services
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
